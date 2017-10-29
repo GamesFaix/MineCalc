4d3d7c3406e5cbc2f2e6faa3d21c358007453cf8
James Faix <jamesthefaix@gmail.com>
2017-10-29 12:34:52 -0400

    Removed dead code

--- MineCalc/Program.cs
+++ MineCalc/Program.cs
@@ -2,8 +2,6 @@
 using System.Windows.Forms;
 using MineCalc.Data;
 using MineCalc.Model;
-using Newtonsoft.Json;
-using static System.Console;
 
 namespace MineCalc
 {
@@ -24,35 +22,6 @@ namespace MineCalc
             form.Calculator = calc;
 
             Application.Run(form);
-        }
-        static void Test()
-        { 
-            var repo = new Repository();
-            var book = repo.LoadRecipeBook();
-            var calc = new Calculator();
-
-            var desiredStack = new BlockStack(new BlockType("Rail"), 1);
-            WriteLine("Desired item");
-            WriteLine(desiredStack);
-            WriteLine();
-
-            var recipe = calc.GetRecipe(book, desiredStack);
-            WriteLine("Recipe");
-            WriteLine(recipe);
-            WriteLine();
-
-            var recipe2 = calc.Expand(recipe, book);
-            WriteLine("Normalized");
-            WriteLine(recipe2);
-            WriteLine();
-
-            Read();
-        }
-
-        static void WriteObject(object obj)
-        {
-            var json = JsonConvert.SerializeObject(obj, Formatting.Indented);
-            WriteLine(json);
         }
     }
 }
